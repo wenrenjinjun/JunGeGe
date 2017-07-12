b0eef669d74d09f10c424332be2c72f6d6665474
闻人金俊 <365553902@qq.com>
2017-07-13 03:59:27 +0800

    jhhjj

---
+++ hello/boot/include/config.h
@@ -0,0 +1,5 @@
+/* Automatically generated - do not edit */
+#define CONFIG_BOARDDIR board/tekkamanninja/mini2440
+#include <config_defaults.h>
+#include <configs/mini2440.h>
+#include <asm/config.h>
---
+++ hello/boot/include/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "Jul 13 2017"
+#define U_BOOT_TIME "03:57:01"
